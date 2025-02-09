d68cc1d1fec698f81bf4d29285c6e731d5a2e8f7
pradeesh-dev <pradeesh.analysis@gmail.com>
2025-02-09 21:22:04 +0530

    Deployment Test

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,6 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 
 export const routes: Routes = [
-    { path: '', component: HomeComponent },
-    { path: '**', redirectTo: '', pathMatch: 'full' }
+    { path: 'portfolio', component: HomeComponent },
+    { path: '**', redirectTo: 'portfolio', pathMatch: 'full' }
 ];
